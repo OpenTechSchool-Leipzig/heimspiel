1139dd52ada6640101ef5d88ee01090ec8153245
markusphil <m.pilipp1991@web.de>
2020-03-21 23:23:08 +0100

    Add filter and search to question list

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,24 @@
+export enum Categories {
+  Gesundheit = 1,
+  Zusammenleben = 2,
+  Produktivität = 3,
+  Haushalt = 4,
+  Zusammenhalt = 5,
+  Beschäftigung = 6
+}
+
+export type CategorieTitles = keyof typeof Categories;
+
+export type QuestCategory = {
+  id: number;
+  title: CategorieTitles;
+  image?: string;
+};
+
 export type Quest = {
   id: number;
   title: string;
-  category: number;
+  category: number; // hier kommt der category type rein!
   text: string;
   flavorText: string;
   score: number;
@@ -11,4 +28,9 @@ export type Quest = {
 export type Member = {
   name: string;
   attributes: string[];
-}
+};
+
+export type QuestFilter = {
+  category: number | null;
+  searchString: string;
+};
