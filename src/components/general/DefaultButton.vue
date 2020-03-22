f2c43134d5661553e9f817db0749b371faebc0f2
markusphil <m.pilipp1991@web.de>
2020-03-22 12:05:13 +0100

    allow button do be link

--- src/components/general/DefaultButton.vue
+++ src/components/general/DefaultButton.vue
@@ -1,10 +1,22 @@
 <template>
-  <button class="button">
+  <router-link v-if="target" to="target" class="button">
+    <slot></slot>
+  </router-link>
+  <button v-else class="button">
     <slot></slot>
   </button>
 </template>
 
-<style lang="scss" scoped>
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component
+export default class DefaultButton extends Vue {
+  @Prop() private target!: boolean | null;
+}
+</script>
+
+<style lang="scss">
 // adjust top padding to center font
 .button {
   padding-top: 0.6em;
