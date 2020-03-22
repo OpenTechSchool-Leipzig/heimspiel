06c868f27e075d250d6786f943a2b2357ebf180c
markusphil <m.pilipp1991@web.de>
2020-03-22 12:04:48 +0100

    gloably register button component

--- src/components/quests/AddQuestion.vue
+++ src/components/quests/AddQuestion.vue
@@ -1,44 +1,60 @@
 <template>
   <div>
-    <label class="label">Wie soll Eure neue Quest heißen?*</label>
-    <div class="control">
-      <input class="input" type="text" v-model="title" />
-    </div>
-
-    <label class="label">In welche Kategorie passt sie am besten?*</label>
-    <div class="control">
-      <div class="select">
-        <select v-model="category">
-          <option selected disabled value>Select poll state</option>
-          <option value>Alle Anzeigen</option>
-          <option v-for="cat in cats" :value="cat.id" :key="cat.id">{{cat.name}}</option>
-        </select>
+    <div class="field">
+      <label class="label">Wie soll Eure neue Quest heißen?*</label>
+      <div class="control">
+        <input class="input" type="text" v-model="title" />
       </div>
     </div>
 
-    <label class="label">Was ist zu tun?*</label>
-    <div class="control">
-      <input class="input" type="text" v-model="text" />
-    </div>
-    <label class="label">Etwas flavor?</label>
-    <div class="control">
-      <input class="input" type="text" v-model="flavorText" />
-    </div>
-    <label class="label">Ein Bild könnte die Quest aufwerten?</label>
-    <div class="control">
-      <input class="input" type="text" v-model="imgURL" />
-    </div>
-    <label class="label">Wieviele Punkte sollte Sie geben?*</label>
-    <div class="control">
-      <div class="select">
-        <select v-model="score">
-          <option selected disabled value>Auswählen</option>
-          <option v-for="val in [1, 3 , 5 , 10 , 12, 15 ,20]" :value="val" :key="val">{{val}}</option>
-        </select>
+    <div class="field">
+      <label class="label">In welche Kategorie passt sie am besten?*</label>
+      <div class="control">
+        <div class="select">
+          <select v-model="category">
+            <option selected disabled value>Select poll state</option>
+            <option value>Alle Anzeigen</option>
+            <option v-for="cat in cats" :value="cat.id" :key="cat.id">{{cat.name}}</option>
+          </select>
+        </div>
       </div>
     </div>
-    <div class="control">
-      <button class="button is-small" @click="add">Hinzufügen</button>
+
+    <div class="field">
+      <label class="label">Was ist zu tun?*</label>
+      <div class="control">
+        <input class="input" type="text" v-model="text" />
+      </div>
+    </div>
+
+    <div class="field">
+      <label class="label">Etwas flavor?</label>
+      <div class="control">
+        <input class="input" type="text" v-model="flavorText" />
+      </div>
+    </div>
+
+    <div class="field">
+      <label class="label">Ein Bild könnte die Quest aufwerten?</label>
+      <div class="control">
+        <input class="input" type="text" v-model="imgURL" />
+      </div>
+    </div>
+    <div class="field">
+      <label class="label">Wieviele Punkte sollte Sie geben?*</label>
+      <div class="control">
+        <div class="select">
+          <select v-model="score">
+            <option selected disabled value>Auswählen</option>
+            <option v-for="val in 6" :value="val" :key="val">{{val}}</option>
+          </select>
+        </div>
+      </div>
+    </div>
+    <div class="field">
+      <div class="control">
+        <DefaultButton @click="add">Hinzufügen</DefaultButton>
+      </div>
     </div>
   </div>
 </template>
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+//general components
+import DefaultButton from "@/components/general/DefaultButton.vue";
 Vue.config.productionTip = false;
+
+// regsiter general components globally
+Vue.component("DefaultButton", DefaultButton);
 
 new Vue({
   router,
