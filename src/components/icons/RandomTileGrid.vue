92630c69dd104040bc079be79d6ed7388803781f
Kristin Fritsch <ich@kristin-fritsch.de>
2020-03-22 15:29:58 +0100

    Rename

--- src/components/icons/RandomTileGrid.vue
+++ src/components/icons/RandomTileGrid.vue
@@ -10,7 +10,7 @@
 
 <script lang="ts">
 import { Component, Vue, Prop } from "vue-property-decorator";
-import CategoryTile from "@/components/icons/CategoryTile.vue";
+import CategoryTile from "@/components/icons/CategoryTile2.vue";
 import { getCatArray } from "../../utility/categories";
 import { CatItem, TilePosition } from "../../types";
 
--- src/components/quests/QuestCard.vue
+++ src/components/quests/QuestCard.vue
@@ -3,7 +3,10 @@
     <div class="card">
       <div class="card-image">
         <figure class="image is-4by3">
-          <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+          <img
+            src="https://bulma.io/images/placeholders/1280x960.png"
+            alt="Placeholder image"
+          />
         </figure>
       </div>
       <div class="card-content">
@@ -28,7 +31,7 @@
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 import { Quest } from "@/types";
-import CategoryTile from "@/components/icons/CategoryTile.vue";
+import CategoryTile from "@/components/icons/CategoryTile2.vue";
 
 @Component({
   components: {
