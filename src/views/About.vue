b33d4e329439df1ce70a6f684caf05cdc778372d
Kristin Fritsch <ich@kristin-fritsch.de>
2020-03-22 22:59:44 +0100

    Update about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,15 +1,45 @@
 <template>
-  <div class="about">
-    <br />
-    <br />
-    <br />
-    <h1>
-      Elisa, Markus, Norma, Nhi, Dominik, Sören and Krissi had fun building
-      this.
-    </h1>
+  <section class="section is-centered">
+    <h2 class="subtitle">About</h2>
+    Dieses Projekt ist im Rahmen von WirVsVirus enstanden.
     <br />
     <p>
-      Special thanks to Robin for mentoring.
+      Mitgemacht haben: Elisa, Markus, Norma, Nhi, Dominik, Sören und Krissi
     </p>
-  </div>
+    <br />
+    <p>
+      Vielen Dank an Robin für die Unterstützung.
+    </p>
+
+    <br />
+    <br />
+    <h2 class="subtitle">Mehr Info's zum Projekt</h2>
+
+    <a
+      href="https://devpost.com/software/heimspiel/"
+      target="_blank"
+      rel="noopener noreferrer"
+      >https://devpost.com/software/heimspiel/</a
+    >
+
+    <br />
+    <br />
+    <br />
+    <h2 class="subtitle">Du willst mitmachen?</h2>
+
+    <p>Hier findest du das Projekt auf Github:</p>
+    <a
+      href="https://github.com/OpenTechSchool-Leipzig/heimspiel"
+      target="_blank"
+      rel="noopener noreferrer"
+      >Frontend</a
+    >
+    <br />
+    <a
+      href="https://github.com/OpenTechSchool-Leipzig/heimspiel-backend"
+      target="_blank"
+      rel="noopener noreferrer"
+      >Backend</a
+    >
+  </section>
 </template>
