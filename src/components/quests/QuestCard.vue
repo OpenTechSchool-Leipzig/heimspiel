fdfb678be9ea8279c4bee2890eb88dd3481ad80e
markusphil <m.pilipp1991@web.de>
2020-03-22 12:05:25 +0100

    Add ctaegory tile component

--- src/components/quests/QuestCard.vue
+++ src/components/quests/QuestCard.vue
@@ -3,16 +3,15 @@
     <div class="card">
       <div class="card-image">
         <figure class="image is-4by3">
-          <img
-            src="https://bulma.io/images/placeholders/1280x960.png"
-            alt="Placeholder image"
-          />
+          <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
         </figure>
       </div>
       <div class="card-content">
         <div>
           <h3 class="title is-4">{{ quest.title }}</h3>
-          <span class="symbol" :class="iconClass">{{ quest.score }}</span>
+          <span class="symbol">
+            <CategoryTile :catID="quest.category.id" :isSmall="true" />
+          </span>
         </div>
         <div class="quest-card-img">
           <img :src="quest.url" />
@@ -29,8 +28,13 @@
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 import { Quest } from "@/types";
+import CategoryTile from "@/components/icons/categoryTile.vue";
 
-@Component
+@Component({
+  components: {
+    CategoryTile
+  }
+})
 export default class QuestCard extends Vue {
   // TODO: fetch quests from API
   @Prop() private quest!: Quest;
