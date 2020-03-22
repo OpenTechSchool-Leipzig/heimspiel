b02e57a4404e78053c0a9aafc9c6dc0f6f77ae6f
markusphil <m.pilipp1991@web.de>
2020-03-22 21:46:31 +0100

    fix button

---
+++ src/views/ScoreReport.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <section class="section is-centered">
+      <h1 class="title">Tages Report</h1>
+      <br />
+      <h2 class="subtitle">Mal schauen, wie {{team.name}} sich heute geschlagen hat</h2>
+    </section>
+    <section class="section"></section>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import QuestCard from "@/components/quests/QuestCard.vue";
+import { Quest } from "@/types";
+
+@Component({})
+export default class Dashboard extends Vue {}
+</script>
