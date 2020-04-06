8903b061c5b95e3c4bb261e8cabbdf31240ce40a
markusphil <m.philipp1991@web.de>
2020-04-06 21:48:54 +0200

    add basic styling to cat input

--- src/components/scorereport/CatInput.vue
+++ src/components/scorereport/CatInput.vue
@@ -1,20 +1,22 @@
 <template>
   <div class="attribute-input">
     <CategoryTile :catID="id" :isSmall="true" />
-    <span>{{ name }}</span>
-    <div class="control">
-      <input
-        class="input"
-        type="number"
-        :value="value"
-        @input="$emit('input', $event.target.value)"
-      />
+    <div class="container-right">
+      <span :class="'category-' + id">{{ name }}</span>
+      <div class="control">
+        <input
+          class="input"
+          type="number"
+          :value="value"
+          @input="$emit('input', $event.target.value)"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Prop, Watch } from "vue-property-decorator";
+import { Component, Vue, Prop } from "vue-property-decorator";
 import CategoryTile from "@/components/icons/CategoryTile.vue";
 
 @Component({
@@ -26,10 +28,20 @@ export default class CatInput extends Vue {
   @Prop() private value!: number;
   @Prop() private id!: number;
   @Prop() private name!: string;
-
-  /*   @Watch("value")
-  valChanged(newVal: number) {
-    this.$emit("input", newVal);
-  } */
 }
 </script>
+
+<style lang="scss" scoped>
+.attribute-input {
+  display: flex;
+  justify-content: stretch;
+  max-width: 600px;
+  margin: 3rem auto;
+}
+.container-right {
+  display: flex;
+  flex-direction: column;
+  margin-left: 2rem;
+  align-items: flex-end;
+}
+</style>
